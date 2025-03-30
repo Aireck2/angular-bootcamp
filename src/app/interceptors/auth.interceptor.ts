2144979163d1cd721af7a2a5a4299916b26b1e23
@erick <erickescribaa@gmail.com>
2025-03-30 01:33:23 -0500

    feat: add auth interceptor

---
+++ src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    const token = localStorage.getItem('adminToken');
+    const authReq = token
+      ? req.clone({
+          setHeaders: { Authorization: `Bearer ${token}` },
+        })
+      : req;
+
+    return next.handle(authReq);
+  }
+}
