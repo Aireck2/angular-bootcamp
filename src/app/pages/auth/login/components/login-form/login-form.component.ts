096cd18b941a72be4fcf587fd71449b98c731cff
@erick <erickescribaa@gmail.com>
2025-03-31 02:32:32 -0500

    chore(admin): add login page

--- src/app/pages/auth/login/components/login-form/login-form.component.ts
+++ src/app/pages/auth/login/components/login-form/login-form.component.ts
@@ -1,5 +1,10 @@
 import { Component, inject, signal } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { NgIcon } from '@ng-icons/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
@@ -7,34 +12,79 @@ import { HlmFormFieldModule } from '@spartan-ng/ui-formfield-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
 import { HlmInputDirective } from '@spartan-ng/ui-input-helm';
 import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { AlertErrorComponent } from '../../../../../components/alert/error-alert.component';
 import { AuthService } from '../../../../../services/auth.service';
-
+import { LoginCredentials } from '../../../../../types/auth.types';
 @Component({
   selector: 'app-login-form',
   imports: [
     HlmButtonDirective,
-    NgIcon,
     HlmIconDirective,
     HlmInputDirective,
-    FormsModule,
     HlmLabelDirective,
     HlmFormFieldModule,
+    NgIcon,
+    FormsModule,
+    ReactiveFormsModule,
+    AlertErrorComponent,
   ],
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.css',
 })
 export class LoginFormComponent {
-  public isLoading = signal(false);
-  private authService = inject(AuthService);
-  private router = inject(Router);
+  protected isLoading = signal(false);
+  protected error = signal({
+    title: '',
+    description: '',
+    show: false,
+  });
+  private _authService = inject(AuthService);
+  private _router = inject(Router);
+  private _fb = inject(FormBuilder).nonNullable;
+
+  loginForm = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  handleError() {
+    this.error.set({
+      title: 'Error',
+      description: 'Invalid email or password',
+      show: true,
+    });
+    setTimeout(
+      () =>
+        this.error.set({
+          title: 'Error',
+          description: 'Invalid email or password',
+          show: false,
+        }),
+      3000
+    );
+    this.isLoading.set(false);
+  }
 
   async login() {
-    await this.send();
-    this.authService.login('admin-secret-token'); // Simulating login
-    this.router.navigate(['/admin']); // Redirect to admin panel
-  }
-  async send() {
     this.isLoading.set(true);
-    setTimeout(() => this.isLoading.set(false), 3000);
+    if (this.loginForm.valid) {
+      const credentials: LoginCredentials = this.loginForm.getRawValue();
+
+      this._authService.login(credentials).subscribe({
+        next: ({ data }) => {
+          if (!data) {
+            this.handleError();
+            return;
+          }
+          this._authService.setValue('adminToken', data.accessToken);
+          this._router.navigate(['/admin']);
+        },
+        error: (error) => {
+          this.handleError();
+        },
+      });
+    } else {
+      this.isLoading.set(false);
+    }
   }
 }
--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { LoginFormComponent } from './components/login-form/login-form.component';
 
 @Component({
@@ -6,4 +7,12 @@ import { LoginFormComponent } from './components/login-form/login-form.component
   imports: [LoginFormComponent],
   templateUrl: './login.component.html',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  titleService = inject(Title);
+  appName = import.meta.env.NG_APP_PREFIX_APP_NAME;
+  constructor() {
+    this.titleService.setTitle(
+      `Admin Sign-in | ${import.meta.env.NG_APP_PREFIX_APP_NAME}`
+    );
+  }
+}
